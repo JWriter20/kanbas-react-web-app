8e570bdfa83c73f8fe34d0629ee8dea672f77b27
Jake Writer <jakewriter@Jakes-MacBook-Pro-2.local>
2023-10-19 23:58:20 -0400

    finished modules

--- src/Kanbas/Courses/Modules/ModuleList.js
+++ src/Kanbas/Courses/Modules/ModuleList.js
@@ -1,37 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import db from "../../Database";
+import "./index.css";
+import { FaEllipsisV, FaCheckCircle, FaGripVertical, FaCaretDown, FaCaretRight, FaPlus } from "react-icons/fa";
 
 function ModuleList() {
   const { courseId } = useParams();
   const modules = db.modules;
+  const [clicked, setClicked] = useState(new Array(modules.length).fill(false));
   return (
-    <ul className="list-group">
-      {
-        modules
-         .filter((module) => module.course === courseId)
-         .map((module, index) => (
-           <li key={index} className="list-group-item">
-             <h3>{module.name}</h3>
-             <p>{module.description}</p>
-             {
-                module.lessons && (
-                    <ul className="list-group">
-                        {
-                            module.lessons.map((lesson, index) => (
-                                <li key={index} className="list-group-item">
-                                    <h4>{lesson.name}</h4>
-                                    <p>{lesson.description}</p>
-                                </li>
-                            ))
-                        }
-                    </ul>
-                )
-             }
-           </li>
-      ))
-      }
+    <ul class="list-group mr-2">
+      {console.log(modules)}
+      {modules.filter((module) => module.course === courseId).map((module, index) => {
+        return <SingleModule props={{index:index, clicked: clicked, setClicked: setClicked}}/>
+      })}
     </ul>
+  );
+}
+
+function handleCarotClick(props) {
+  props.clicked[props.index] = !props.clicked[props.index];
+  props.setClicked([...props.clicked]);
+}
+
+function SingleModule(props) {
+  props = props.props;
+  const modules = db.modules;
+  if (props.clicked[props.index]) {
+    return (
+      <div>
+            <li className="list-group-item rounded-top py-3 b-green" style={{backgroundColor: "lightgrey", textAlign: "left"}}>
+              <FaGripVertical className="m-1 mr-2" style={{color: "grey"}} /> 
+              <div className="btn button" onClick={() => handleCarotClick(props)}><FaCaretDown />{modules[props.index].name}</div>
+              <FaEllipsisV className="right-icon" style={{color: "black"}} />
+              <FaPlus className="right-icon mx-3"/>
+              <FaCheckCircle className="right-icon text-success" />
+            </li>
+          {
+            modules[props.index].lessons.map((lesson, index) => {
+              return (<li className={`list-group-item py-3 b-green ${index == modules[props.index].lessons.length - 1 && 'rounded-bottom'}`} style={{backgroundColor: "lightgrey", textAlign: "left"}}>
+              <FaGripVertical className="m-1 mr-2" style={{color: "grey"}} /> 
+               <FaEllipsisV className="right-icon" style={{color: "black"}} />
+               <FaCheckCircle className="right-icon text-success" />
+               <a className="indent">{lesson.name}</a>
+              </li>)
+            })
+          }
+
+      </div>
+    );
+  }
+
+  return (
+    <li className="list-group-item rounded my-3 py-3 b-green" style={{backgroundColor: "lightgrey", textAlign: "left"}}>
+          <FaGripVertical className="m-1 mr-2" style={{color: "grey"}} /> 
+          <div className="btn button" onClick={() => handleCarotClick(props)}><FaCaretRight />{modules[props.index].name}</div>
+           <FaEllipsisV className="right-icon" style={{color: "black"}} />
+           <FaPlus className="right-icon mx-3"/>
+           <FaCheckCircle className=" right-icon text-success" />
+        </li>
+    
   );
 }
 export default ModuleList;
--- src/Kanbas/Courses/Modules/index.js
+++ src/Kanbas/Courses/Modules/index.js
@@ -1,8 +1,19 @@
 import ModuleList from "./ModuleList";
 function Modules() {
   return (
-    <div>
-      <h2>Modules</h2>
+    <div className="container">
+        <div class="btn m-1 btn-secondary">Collapse All</div>
+        <div class="btn m-1 btn-secondary">View Progress</div>
+        <select class="btn m-1 btn-secondary">
+          <option>Publish All</option>
+          <option>Publish All modules and items</option>
+          <option>Publish modules only</option>
+          <option>UnPublish All</option>
+        </select>
+
+        <div class="btn m-1 btn-danger">+ Module</div>
+
+        <hr />
       <ModuleList />
     </div>
   );
