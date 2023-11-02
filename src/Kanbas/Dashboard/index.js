4a8adcbe6de0c3c2dad204c7c9defc4b484d5bfc
Jake Writer <jakewriter@Jakes-MacBook-Pro-2.local>
2023-11-02 19:20:36 -0400

    finished with dashboard

--- src/Kanbas/Dashboard/index.js
+++ src/Kanbas/Dashboard/index.js
@@ -6,85 +6,69 @@ import { React, useState } from "react";
 import { FaTrash, FaEdit } from "react-icons/fa";
 import { Modal, Button, Form } from 'react-bootstrap';
 
+const course = {
+  name: "New Course",      number: "New Number",
+  startDate: "2023-09-10", endDate: "2023-12-15",
+};
 
+const ADD_COURSE = "ADD";
+const EDIT_COURSE = "EDIT";
 
 function Dashboard() {
-  const exampleCourse = {
-    name: "New Course",      number: "New Number",
-    startDate: "2023-09-10", endDate: "2023-12-15",
-  };
+  const [courses, setCourses] = useState(db.courses);
+  const [modalData, setModalData] = useState({active: false, title: "", modalType: ADD_COURSE, course: course});
 
-  const [state, setState] = useState({courses: db.courses, modal: {active: false, title: "", submitFunc: () => null}, course: exampleCourse});
-
-  const addNewCourse = () => {
-    setState((prevState) => {
-      const updatedCourses = {
-        ...prevState,
-        courses: [
-          ...prevState.courses,
-          {
-            ...prevState.course,
-            _id: new Date().getTime().toString(),
-          },
-        ],
-      };
-      return updatedCourses;
-    });
-  };
-
-  console.log(state);
-
+  console.log(modalData)
   return (
     <div className="container">
-      { state.modal ? createModal(state, setState) : null}
+      { modalData.active ? createModal(modalData, setModalData, courses, setCourses) : null}
       <h2>Dashboard</h2>
       <hr />
       <div className="container">
-        <h3>Published Courses({state.courses.length})<div className="btn btn-primary float-end" onClick={() => setState({...state, modal: {...state.modal, active: true, title: "Add Course", submitFunc: addNewCourse}})}>Add Course +</div></h3>
+        <h3>Published Courses({courses.length})<div className="btn btn-primary float-end" onClick={
+          () => setModalData({...modalData, active: true, title: "Add Course", modalType: ADD_COURSE, course: course})
+        }>Add Course +</div></h3>
         <hr />
         <div className="row dashboard-row-margin">
-            {makeRows(state, setState)}
+            {makeRows(courses, setCourses, modalData, setModalData)}
         </div>
       </div>
     </div>
   );
 }
 
-function makeCard(state, setState, newCourse) {
-  const deleteCourse = (courseId) => {
-    setState({...state, courses: state.courses.filter((course) => course._id !== courseId)});
+function makeCard(course, courses, setCourses, modalData, setModalData) {
+  const deleteCourse = () => {
+    setCourses(courses.filter((c) => c._id !== course._id));
   };
 
-  const updateCourse = (courseId) => {
-    const newState = {...state, courses: state.courses.map((course) => course._id === courseId ? state.course : course)};
-    console.log("below");
-    console.log(newState);
-    setState(newState);
-  };
+  const showModal = () => {
+    setModalData({...modalData, active: true, title: "Edit Course", modalType: EDIT_COURSE, course: course})
+  }
 
-  return <Link key={newCourse._id} to={`/Kanbas/Courses/${newCourse._id}`} className="list-group-item">
+  return <Link key={course._id} to={`/Kanbas/Courses/${course._id}`} className="list-group-item">
     <div className="col-12 col-md-6 col-xl-3">
             <div className="card shadow dashboard-card" style={{width: "270px"}}>
               <div className="card-body">
                   <p className="card-title">
-                      {newCourse.name}</p>
+                      {course.name}</p>
                   <p className="card-text">
-                      {newCourse.number}.{newCourse._id}</p>
+                      {course.number}.{course._id}</p>
                   <p className="card-subtitle">
-                      {newCourse.startDate} | {newCourse.endDate}</p>
+                      {course.startDate} | {course.endDate}</p>
                   <div className="btn px-1 py-0">
                   <FaFileAlt style={{color: "black", fontSize: "20px"}}/>
                   </div>
                   <div className="btn py-0 px-1 float-end" onClick={(event) => { 
-                    event.preventDefault(); 
-                    deleteCourse(newCourse._id);
-                  }}>
+                      event.preventDefault(); 
+                      deleteCourse();
+                    }}>
                     <FaTrash style={{color: "red", fontSize: "20px"}} />
                   </div>
                   <div className="btn py-0 px-1 float-end" onClick={(event) => { 
-                    event.preventDefault(); 
-                    setState({...state, course: newCourse, modal: {active: true, title: "Edit Course", submitFunc: () => updateCourse(newCourse._id)}})
-                  }}>
+                      event.preventDefault(); 
+                      showModal();
+                    }}>
                     <FaEdit style={{color: "black", fontSize: "21px"}}/>
                   </div>
               </div>
@@ -93,17 +77,16 @@ function makeCard(state, setState, newCourse) {
   </Link>
 }
 
-function makeRows(state, setState) {
+function makeRows(courses, setCourses, modalData, setModalData) {
   let rows = [];
-  const courses = state.courses;
 
   for (let i = 0; i < courses.length; i += 4) {
     rows.push(
       <div className="list-group list-group-horizontal">
-        {i < courses.length ? makeCard(state, setState, courses[i]) : ''}
-        {i + 1 < courses.length ? makeCard(state, setState, courses[i + 1]): ''}
-        {i + 2 < courses.length ? makeCard(state, setState, courses[i + 2]): ''}
-        {i + 3 < courses.length ? makeCard(state, setState, courses[i + 3]): ''}
+        {i < courses.length ? makeCard(courses[i], courses, setCourses, modalData, setModalData) : ''}
+        {i + 1 < courses.length ? makeCard(courses[i + 1], courses, setCourses, modalData, setModalData): ''}
+        {i + 2 < courses.length ? makeCard(courses[i + 2], courses, setCourses, modalData, setModalData): ''}
+        {i + 3 < courses.length ? makeCard(courses[i + 3], courses, setCourses, modalData, setModalData): ''}
       </div>
     );
 
@@ -113,50 +96,68 @@ function makeRows(state, setState) {
     
 }
 
-function createModal(state, setState) {
-  const newCourse = state.course;
+function createModal(modalData, setModalData, courses, setCourses) {
 
-  const setCourse = (course) => {
-    setState({ ...state, course: course });
+  const addNewCourse = () => {
+    setCourses([...courses,
+              { ...modalData.course,
+                _id: new Date().getTime() }]);
   };
+
+  const updateCourse = () => {
+    const newState = courses.map((c) => c._id === modalData.course._id ? modalData.course : c);
+    setCourses(newState);
+  };
+
+
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    state.modal.submitFunc();
+    switch (modalData.modalType) {
+      case ADD_COURSE: addNewCourse(); break;
+      case EDIT_COURSE: updateCourse(); break;
+      default:
+        break;
+    }
     handleClose();
   };
 
   const handleClose = () => {
-    setState({ ...state, modal: { ...state.modal, active: false } });
+    setModalData({...modalData, active: false});
+  };
+
+  const setCourse = (course) => {
+    setModalData({...modalData, course: course});
+
   };
 
   return (
     <>
-      <Modal show={state.modal.active} onHide={handleClose}>
+      <Modal show={modalData.active} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>{state.modal.title}</Modal.Title>
+          <Modal.Title>{modalData.title}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Form id="createCourseModal">
             <Form.Group controlId="courseName">
               <Form.Label>Course Name</Form.Label>
-              <Form.Control type="text" onChange={(e) => setCourse({ ...newCourse, name: e.target.value }) } value={newCourse.name} name="name" placeholder="Name your Course" />
+              <Form.Control type="text" onChange={(e) => setCourse({ ...modalData.course, name: e.target.value }) } value={modalData.course.name} name="name" placeholder="Name your Course" />
             </Form.Group>
 
             <Form.Group controlId="courseNum">
               <Form.Label>Course Name</Form.Label>
-              <Form.Control type="text" onChange={(e) => setCourse({ ...newCourse, number: e.target.value }) } value={newCourse.number} name="courseNumber" placeholder="Course number" />
+              <Form.Control type="text" onChange={(e) => setCourse({ ...modalData.course, number: e.target.value }) } value={modalData.course.number} name="courseNumber" placeholder="Course number" />
             </Form.Group>
 
             <Form.Group controlId="formDimensions">
               <div className="row mt-1">
                   <div className="col-sm-6">
                       <Form.Label>Start Date</Form.Label>
-                      <Form.Control value={newCourse.startDate} onChange={(e) => setCourse({ ...newCourse, startDate: e.target.value }) } name="startDate" type="date" placeholder="Course start-date" />
+                      <Form.Control value={modalData.course.startDate} onChange={(e) => setCourse({ ...modalData.course, startDate: e.target.value }) } name="startDate" type="date" placeholder="Course start-date" />
                   </div>
                   <div className="col-sm-6">
                       <Form.Label>End Date</Form.Label>
-                      <Form.Control value={newCourse.endDate} onChange={(e) => setCourse({ ...newCourse, endDate: e.target.value }) } name="endDate" type="date" placeholder="Course end-date" />
+                      <Form.Control value={modalData.course.endDate} onChange={(e) => setCourse({ ...modalData.course, endDate: e.target.value }) } name="endDate" type="date" placeholder="Course end-date" />
                   </div>
               </div>
             </Form.Group>
